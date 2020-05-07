406bf93620d566c89f1f15a76c2ead7a3a12f89d
EmilJK <emjoka18@gmail.com>
2020-05-07 16:50:45 +0200

    Ai Start

---
+++ Assets/Prototyping/Scripts/AiScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiScript : MonoBehaviour
+{
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
