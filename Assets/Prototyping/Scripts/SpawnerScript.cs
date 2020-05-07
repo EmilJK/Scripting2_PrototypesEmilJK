53647b519e8e307d203daed3b7360dfa7ee178c8
EmilJK <emjoka18@gmail.com>
2020-05-07 16:30:51 +0200

    ObjectSpawner

---
+++ Assets/Prototyping/Scripts/SpawnerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject objToSpawn;
+    public float spawnTime;
+    public float spawnTimeRandom;
+
+    private float spawnTimer;
+
+    private void Start()
+    {
+        ResetSpawnerTimer();
+    }
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if(spawnTimer <= 0f)
+        {
+            Instantiate(objToSpawn, transform.position, Quaternion.identity);
+            ResetSpawnerTimer();
+        }
+    }
+
+
+    void ResetSpawnerTimer()
+    {
+        spawnTimer = (spawnTime + Random.Range(0, spawnTimeRandom * 100f) / 100f);
+    }
+}
