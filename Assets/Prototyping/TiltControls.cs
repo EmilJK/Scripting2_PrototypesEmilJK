2f08470093d0f36613b0a2b5bc94c62e87eb108d
EmilJK <emjoka18@gmail.com>
2020-05-07 15:51:55 +0200

    Prototype1_Done

---
+++ Assets/Prototyping/TiltControls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltControls : MonoBehaviour
+{
+
+    public float tiltSpeed = 1;
+
+    void FixedUpdate()
+    {
+        transform.Translate(Input.acceleration.x * tiltSpeed * Time.deltaTime, Input.acceleration.y * tiltSpeed * Time.deltaTime, 0);
+    }
+}
