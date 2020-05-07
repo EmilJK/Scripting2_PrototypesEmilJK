64457585308d4a0bea59b863cb9df9ab8220c94c
EmilJK <emjoka18@gmail.com>
2020-05-07 18:27:57 +0200

    Ai

--- Assets/Prototyping/Scripts/AiScript.cs
+++ Assets/Prototyping/Scripts/AiScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class AiScript : MonoBehaviour
 {
+    public float speed = 5;
+    bool movingRight = true;
+    bool movingUp = true;
 
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Wall")
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+            
+        }
+
+        if(other.gameObject.tag == "Ceiling")
+        {
+            if (movingUp == true)
+            {
+                transform.eulerAngles = new Vector3(-180, 0, 0);
+                movingUp = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingUp = true;
+            }
+        }
+
+        if (other.gameObject.tag == "Player")
         {
             Destroy(gameObject);
         }
